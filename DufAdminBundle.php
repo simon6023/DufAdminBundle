f2b7306746f06f7b04a530d0a7abecfccc375813
simonduflosuzik <simon.duflos@uzik.com>
2016-10-04 18:51:13 +0200

    bundle dependencies

--- DufAdminBundle.php
+++ DufAdminBundle.php
@@ -3,7 +3,27 @@
 namespace Duf\AdminBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use AshleyDawson\MultiBundle\AbstractMultiBundle;
 
-class DufAdminBundle extends Bundle
+class DufAdminBundle extends AbstractMultiBundle
 {
+	protected static function getBundles()
+	{
+		return array(
+				'prod' 	=> array(
+		            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+		            new Bmatzner\FontAwesomeBundle\BmatznerFontAwesomeBundle(),
+		            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+		            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+		            new Lexik\Bundle\TranslationBundle\LexikTranslationBundle(),
+		            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+		            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+		            new Duf\CoreBundle\DufCoreBundle(),
+		            new Duf\MessagingBundle\DufMessagingBundle(),
+				),
+				'dev' 	=> array(
+					new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+				),
+			);
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# DufAdminBundle
+# Installation
+
+Add `simonduflos/dufadminbundle` to your `composer.json`
+
+```json
+{
+	"simonduflos/dufadminbundle":"dev-master"
+}
+```
+
+Run `composer.phar update` to install the bundle and its dependencies
