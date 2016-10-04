e2e1e988ea326f2f8b195f9410ca099432ac2862
simonduflosuzik <simon.duflos@uzik.com>
2016-10-04 19:07:00 +0200

    update bundle dependencies class

--- DufAdminBundle.php
+++ DufAdminBundle.php
@@ -11,18 +11,18 @@ class DufAdminBundle extends AbstractMultiBundle
 	{
 		return array(
 				'prod' 	=> array(
-		            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
-		            new Bmatzner\FontAwesomeBundle\BmatznerFontAwesomeBundle(),
-		            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-		            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-		            new Lexik\Bundle\TranslationBundle\LexikTranslationBundle(),
-		            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
-		            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-		            new Duf\CoreBundle\DufCoreBundle(),
-		            new Duf\MessagingBundle\DufMessagingBundle(),
+		            new \Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+		            new \Bmatzner\FontAwesomeBundle\BmatznerFontAwesomeBundle(),
+		            new \FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+		            new \Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+		            new \Lexik\Bundle\TranslationBundle\LexikTranslationBundle(),
+		            new \HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+		            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
+		            new \Duf\CoreBundle\DufCoreBundle(),
+		            new \Duf\MessagingBundle\DufMessagingBundle(),
 				),
 				'dev' 	=> array(
-					new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+					new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
 				),
 			);
 	}
