b9a780222a5ead0888df4dafb7c71fba630bb454
simonduflosuzik <simon.duflos@uzik.com>
2017-09-08 14:36:31 +0200

    fix PrePersist

--- Entity/DufAdminAbstractEntity.php
+++ Entity/DufAdminAbstractEntity.php
@@ -10,13 +10,13 @@ use Doctrine\ORM\Mapping as ORM;
  */
 abstract class DufAdminAbstractEntity
 {
-    /** @ORM\prePersist */
+    /** @ORM\PrePersist */
     public function setCreatedAtValue()
     {
         $this->created_at = new \DateTime();
     }
 
-    /** @ORM\preUpdate */
+    /** @ORM\PreUpdate */
     public function setUpdatedAtValue()
     {
         $this->updated_at = new \DateTime();
