9a693287198148f569b8827bed4414307341e8f9
simonduflosuzik <simon.duflos@uzik.com>
2017-10-16 12:17:32 +0200

    update doctrine schema from admin

--- Controller/CacheController.php
+++ Controller/CacheController.php
@@ -3,8 +3,11 @@
 namespace Duf\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 class CacheController extends Controller
 {
@@ -17,6 +20,26 @@ class CacheController extends Controller
                 $session        = $this->get('session')->getFlashBag()->add('notice', 'Doctrine Cache cleared');
                 break;
         }
+
+        return $this->redirect($this->generateUrl('duf_admin_homepage'));
+    }
+
+    public function updateDoctrineSchemaAction()
+    {
+        $kernel         = $this->get('kernel');
+        $application    = new Application($kernel);
+
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput(array(
+            'command'   => 'doctrine:schema:update',
+            '--force'   => true,
+        ));
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        $session = $this->get('session')->getFlashBag()->add('notice', $output->fetch());
 
         return $this->redirect($this->generateUrl('duf_admin_homepage'));
     }
